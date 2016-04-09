280a516568e7f765c7523d6a71416a8c2b98c892
James McElwain <jcm@Jamess-MacBook-Pro.local>
2016-04-09 11:37:18 -0500

    init default quil template and add midi-clj library from overtone

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject quil-midi "0.1.0-SNAPSHOT"
+  :description "Quil with Ableton MIDI Interop"
+  :url ""
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [overtone/midi-clj "0.1"]
+                 quil "2.4.0"]])
