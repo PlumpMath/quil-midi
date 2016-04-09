d3a8c3d6b8c88a626475d37a15b0298b3d509ec6
James McElwain <jcm@Jamess-MacBook-Pro.local>
2016-04-09 11:41:57 -0500

    fix syntax error in project.clj

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [overtone/midi-clj "0.1"]
-                 quil "2.4.0"]])
+                 [quil "2.4.0"]])
