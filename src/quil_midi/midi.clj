8080c8e36c01093456da966ed6f71af848406dd1
James McElwain <jcm@Jamess-MacBook-Pro.local>
2016-04-09 13:27:25 -0500

    looking for iac device properly

--- src/quil_midi/midi.clj
+++ src/quil_midi/midi.clj
@@ -1,4 +1,13 @@
 (ns quil-midi.midi (:require [overtone.midi :as midi])) 
 
+(def iac-device-name "virtual-midi")
+(def devices (midi/midi-devices))
+
+(def iac?
+  (fn [device]
+    (= (get device :name) iac-device-name)))
+
 (defn -main []
-  (print (midi/midi-devices)))
+  (print (if (seq (filter iac? devices))
+           "IAC device located"
+           (str "Cannot locate IAC device with name " iac-device-name))))
