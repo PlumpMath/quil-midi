2c04b23dbaf8bdfd2c64e4b7aa0ea905d5e9fa00
James McElwain <jmcelwain@gmail.com>
2016-04-10 16:10:19 -0500

    convert sketch to something simple to test midi functionality better-- still running into problems with how the event handler on the jaa interop is working, not super clean, need to make the interface better, maybe look at midibus

--- src/quil_midi/core.clj
+++ src/quil_midi/core.clj
@@ -3,49 +3,26 @@
             [quil.middleware :as m]
             [quil-midi.midi :as midi]))
 
-(def bg-col (atom 0))
+(def state (atom 0))
+
+(midi/listener (fn [msg]
+                 (swap! state (fn [x]
+                                (c-r (:data2 msg))))))
 
 (defn setup []
-  (midi/listener (fn [x] (let [vol (:data2 x)] (swap! bg-col (fn [x] vol)))))
-  ; Set frame rate to 30 frames per second.
-  (q/frame-rate 30)
-  ; Set color mode to HSB (HSV) instead of default RGB.
-  (q/color-mode :hsb)
-  ; setup function returns initial state. It contains
-  ; circle color and position.
-  {:color 0
-   :angle 0})
+  (q/background 0)
+  (q/frame-rate 10))
 
-(defn update-state [state]
-  ; Update sketch state by changing circle color and position.
-  {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.1)})
-
-(defn draw-state [state]
-  ; Clear the sketch by filling it with light-grey color.
-  (q/background @bg-col)
-  ; Set circle color.
-  (q/fill (:color state) 255 255)
-  ; Calculate x and y coordinates of the circle.
-  (let [angle (:angle state)
-        x (* 150 (q/cos angle))
-        y (* 150 (q/sin angle))]
-    ; Move origin point to the center of the sketch.
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
-      ; Draw the circle.
-      (q/ellipse x y 100 100))))
+(defn draw []
+  (print @state)
+  (q/background @state))
 
 (q/defsketch quil-midi
-  :title "You spin my circle right round"
-  :size [500 500]
-  ; setup function called only once, during sketch initialization.
+  :size [100, 100]
   :setup setup
-  ; update-state is called on each iteration before draw-state.
-  :update update-state
-  :draw draw-state
-  :features [:keep-on-top]
-  ; This sketch uses functional-mode middleware.
-  ; Check quil wiki for more info about middlewares and particularly
-  ; fun-mode.
-  :middleware [m/fun-mode])
+  :draw draw)
+
+;; util
+
+(defn c-r [x]
+  (x * 255) / 127)
