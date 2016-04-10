6ed7f7ee7a2d3cc4a0ac8ffe9dceddd9da113a78
James McElwain <jmcelwain@gmail.com>
2016-04-09 21:47:38 -0500

    device can now be closed, although prevents re-listening

--- src/quil_midi/midi.clj
+++ src/quil_midi/midi.clj
@@ -1,7 +1,26 @@
-(ns quil-midi.midi
-  (:import
-   (java.sound.midi Sequencer Synthesizer
-                    MidiSystem MidiDevice Reciever Transmitter MidiEvent
-                    MidiMessage ShortMessage SysexMessage
-                    InvalidMidiDataException MidiUnavailableException
-                    MidiDevice$Info)))
+(ns quil-midi.midi (:require [overtone.midi :as midi])) 
+
+(def iac-device-name "virtual-midi")
+(def devices (midi/midi-devices))
+
+(def iac?
+  (fn [device]
+    (= (get device :name) iac-device-name)))
+
+(defn check-iac[]
+  (print (if (seq (filter iac? devices))
+           "IAC device located"
+           (str "Cannot locate IAC device with name " iac-device-name))))
+
+; Delete this
+(defn device []
+  (:device (midi/midi-in iac-device-name)))
+
+(defn close [device]
+  (.close device))
+
+(defn listener []
+  "Returns a device that can be closed"
+  (let [device (midi/midi-in iac-device-name)]
+    (midi/midi-handle-events device (fn [msg] (print msg)))
+    (:device device)))
