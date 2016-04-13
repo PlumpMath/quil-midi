52a0ae2de0b3b00cc43da3c110a2c1d9d50a8511
James McElwain <jmcelwain@gmail.com>
2016-04-12 21:19:14 -0500

    complex sketch working

--- src/quil_midi/core.clj
+++ src/quil_midi/core.clj
@@ -6,9 +6,11 @@
    [quil-midi.midi :as midi])
   (:import [processing.video Capture]))
 
-(declare midi-to-255 log-msg update-atom)
+(declare midi-to-255 log-msg update-atom midi-to-height)
 
-(def state (atom {:val 0}))
+(def cc0 (atom {:val 0}))
+(def cc1 (atom {:val 0}))
+(def cc2 (atom {:val 0}))
 (def camera (atom {:instance nil}))
 
 ;; DISPATCH
@@ -16,10 +18,12 @@
   [midi-msg]
   (let [{n :note v :velocity :as msg} midi-msg]
     (match n
-        0 (update-atom v))))
+           0 (update-atom v cc0)
+           1 (update-atom v cc1)
+           2 (update-atom v cc2))))
 
 
-(defn update-atom [val]
+(defn update-atom [val state]
     (if (not= val (:val @state))
       (swap! state assoc :val val)
       nil))
@@ -27,19 +31,23 @@
 (midi/listener dispatch-midi-event)
 
 (defn setup []
-  (def cam (Capture. (quil.applet/current-applet) 400 400 (first (Capture/list))))
-  (swap! camera assoc :instance cam)
-  (.start (:instance @camera))
   (q/frame-rate 10))
 
 (defn draw []
-  (if (.available (:instance @camera))
-    (.read (:instance @camera)))
-
-  (q/image (:instance @camera) 0 0 ))
+  (def h (q/random 0 400))
+  (q/background (midi-to-255 (:val @cc0)) (midi-to-255 (:val @cc1)) (midi-to-255 (:val @cc2)))
+  (q/stroke 255)
+  (q/stroke-weight 10)
+  (q/line 0 (midi-to-height (:val @cc2))  800 (midi-to-height (:val @cc0)))
+  (q/line 0 (midi-to-height (:val @cc1))  800 (midi-to-height (:val @cc1)))
+  (q/line 0 (midi-to-height (:val @cc0))  800 (midi-to-height (:val @cc2)))
+  (q/stroke-weight 2)
+  (q/stroke 0)
+  ;;(q/line (q/random 0 (q/height)) (q/random 0 (q/height)) (q/random 0 (q/width)) (q/random 0 (q/width)))
+  )
 
 (q/defsketch quil-midi
-  :size [400, 400]
+  :size [800, 400]
   :setup setup
   :features [:keep-on-top]
   :renderer :p3d
@@ -49,6 +57,9 @@
 
 (defn midi-to-255 [x]
   (/ (* x 255) 127))
+
+(defn midi-to-height [x]
+  (/ (* x (q/height)) 127))
 
 ;; main
 
