a03a6e9619b5b70e6986d180d8d6bb36ea51f3df
James McElwain <jmcelwain@gmail.com>
2016-04-28 20:38:43 -0500

    only defs nec to make vel print to screen

--- src/quil_midi/midi.clj
+++ src/quil_midi/midi.clj
@@ -1,35 +1,14 @@
 (ns quil-midi.midi (:require [overtone.midi :as midi]
                              [clojure.core.async :refer [go go-loop chan sliding-buffer >! >!! <! <!!]]))
 
-
-(def devices (midi/midi-devices))
-
-(defn iac?
-  [device]
-  (= (get device :name) iac-device-name))
-
-(defn check-iac[]
-  (print (if (seq (filter iac? devices))
-           "IAC device located"
-           (str "Cannot locate IAC device with name " iac-device-name))))
-
-; Delete this
-(defn get-input-device []
-  (:device (midi/midi-in iac-device-name)))
-
-(defn close
-  [device]
-  (.close device))
+(def sliding-chan (chan (sliding-buffer 1)))
+(def iac-device-name "virtual-midi") 
 
 (defn listener []
   "Returns a device that can be closed"
   (let [device (midi/midi-in iac-device-name)]
     (midi/midi-handle-events device (fn [x] (>!! sliding-chan x))
     (:device device))))
-
-(def sliding-chan (chan (sliding-buffer 1)))
-(def iac-device-name "virtual-midi") 
-
 (def listener-device (listener))
 
 (go-loop []
