62ad5a885b79090078673d73db63edb09fa89f45
James McElwain <jmcelwain@gmail.com>
2016-04-28 20:59:04 -0500

    core calls midi sucessfully to change bg in psketch

--- src/quil_midi/core.clj
+++ src/quil_midi/core.clj
@@ -7,46 +7,28 @@
    [quil-midi.midi :as midi])
   (:import [processing.video Capture]))
 
-(declare midi-to-255 log-msg update-atom midi-to-height)
-
-(def cc0 (atom {:val 0}))
-(def cc1 (atom {:val 0})) 
-(def cc2 (atom {:val 0}))
-(def camera (atom {:instance nil}))
-
-;; DISPATCH
-(defn dispatch-midi-event
-  [midi-msg]
-  (let [{n :note v :velocity :as msg} midi-msg]
-    (match n
-           0 (update-atom v cc0)
-           1 (update-atom v cc1)
-           2 (update-atom v cc2))))
-
-
-(defn update-atom [val state]
-    (if (not= val (:val @state))
-      (swap! state assoc :val val)
-      nil))
-
+(declare midi-to-255 midi-chan setup draw midi-to-height)
 (def midi-chan (midi/listener))
 
 (defn setup []
-  (q/frame-rate 10))
+  (q/frame-rate 10)
+  0)
 
-(defn draw []
-  (q/background (midi-to-255 (:val @cc0)))
-  (doseq [x (range 1 (q/width))
-          y (range 1 (q/height))]
-    (if (= (mod x 2) 0)
-      (q/stroke (:val @cc0))
-      )))
+
+(defn update-state [state]
+  (:velocity (<!! midi/sliding-chan))
+  )
+
+(defn draw-state [state]
+  (q/background state)
+  )
 
 (q/defsketch quil-midi
   :size [400 400]
+  :middleware [m/fun-mode]
   :setup setup
-  :renderer :p3d
-  :draw draw)
+  :update update-state
+  :draw draw-state)
 
 ;; util
 (defn midi-to-255 [x]
--- src/quil_midi/midi.clj
+++ src/quil_midi/midi.clj
@@ -4,17 +4,14 @@
 (def sliding-chan (chan (sliding-buffer 1)))
 (def iac-device-name "virtual-midi") 
 
+;(def loop-print (go-loop []
+;    (println (:data2 (<! sliding-chan)))
+;    (recur)))
+
+
 (defn listener []
   "Returns a device that can be closed"
   (let [device (midi/midi-in iac-device-name)]
     (midi/midi-handle-events device (fn [x] (>!! sliding-chan x))
     (:device device))))
-(def listener-device (listener))
-
-(go-loop []
-    (println (:data2 (<! sliding-chan)))
-    (recur))
-
-(defn log-msg [msg]
-  (print (str "_ " msg  "\n")))
 
