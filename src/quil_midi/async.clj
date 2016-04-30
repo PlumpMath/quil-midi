3a43407996eff3bbcbb280a82376eb48732ead55
James McElwain <jmcelwain@gmail.com>
2016-04-30 18:27:02 -0500

    remove async file

--- src/quil_midi/async.clj
+++
@@ -1,7 +0,0 @@
-(ns quil-midi.async
-  (:require
-   [clojure.core.async :refer [sliding-buffer chan go <! <!! >! >!!]]))
- 
-(def midi-chan (chan (sliding-buffer 10)))
-
-(go (>! midi-chan "value"))
